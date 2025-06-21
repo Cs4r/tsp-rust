d8d8db76d35958fda0f348a43cc8990a82e35591
César Aguilera <cesar.aguilera.p@gmail.com>
2025-06-21 20:20:27 +0200

    refactor

--- bin/random_search.rs
+++ bin/random_search.rs
@@ -1,8 +1,13 @@
 use std::env;
-use tsp_rust::utils::validate_cli_arguments;
+use tsp_rust::utils::{
+    get_number_of_iterations, get_problem_size, read_distance_matrix, validate_cli_arguments,
+};
 
 fn main() {
     let args: Vec<String> = env::args().collect();
     validate_cli_arguments(&args, "Random Search");
 
+    let distance_matrix = read_distance_matrix(&args[1]);
+    let problem_size = get_problem_size(&distance_matrix);
+    let iterations = get_number_of_iterations(&distance_matrix);
 }
